a8f3ba26c50de030ef422e351ec27d866eea1378
Unknown <asc656@nyu.edu>
2019-04-09 11:11:55 -0400

    cutscene scripts

---
+++ DC Prototype/Assets/Scripts/CutsceneScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneScript : MonoBehaviour
+{
+    public List<string> txts = new List<string>();
+
+    public string txt1;
+    public string txt2;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt1 = "hello";
+        txts.Add(txt1);
+
+        txt2 = "some more text";
+        txts.Add(txt2);
+    }
+
+}
