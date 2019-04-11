06bc38ef3282c7ca1f9e92498706b1a22f192575
Unknown <rmh472@nyu.edu>
2019-04-11 11:29:57 -0400

    Auto stash before merge of "R-Dawg" and "origin/annamaria"

--- DC Prototype/Assets/Scripts/Catapult.cs
+++ DC Prototype/Assets/Scripts/Catapult.cs
@@ -27,14 +27,22 @@ public class Catapult : MonoBehaviour
             GameObject current = manager.insideHole[i];
             if (current.GetComponent<Eatable>().launchable)
             {
+                //turn the game object back on
                 current.gameObject.SetActive(true);
+
+                //set its position and rotation to a resting state
                 current.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
                 current.transform.rotation = Quaternion.Euler(0, 0, 0);
                 current.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+                //yeet that bitch
                 current.GetComponent<Rigidbody>().AddForce(Vector3.up * launchForce, ForceMode.Impulse);
+
+                //change the hole accordingly
                 manager.insideHole.Remove(current);
                 manager.Shrink();
                 manager.holeSize -= current.GetComponent<Eatable>().calories;
+
                 break;
             }
         }
