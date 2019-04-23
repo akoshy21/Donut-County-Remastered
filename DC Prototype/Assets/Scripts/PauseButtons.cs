44fbb9f976e4ed5c9b092444fdc659ed4d1917b9
Unknown <asc656@nyu.edu>
2019-04-23 12:33:38 -0400

    pause screen beginnings

---
+++ DC Prototype/Assets/Scripts/PauseButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButtons : MonoBehaviour
+{
+    
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        GetComponent<RectTransform>().sizeDelta = new Vector2();
+    }
+}
