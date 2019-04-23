ea937689dd197efcd1be88c4e6dbee5b4e9fabd8
Annamaria Koshy <ak6429@nyu.edu>
2019-04-22 22:58:12 -0400

    Commented, a couple of bug fixes, etc

--- DC Prototype/Assets/Scripts/Trigger.cs
+++ DC Prototype/Assets/Scripts/Trigger.cs
@@ -18,6 +18,8 @@ public class Trigger : MonoBehaviour
         // water to the list of objects in the hole
         if (col.gameObject.tag.Equals("water"))
         {
+
+            Debug.Log("watered: " + col.gameObject.name);
             hm.GetComponent<HoleManager>().waterFill = true;
             Debug.Log("waterfill " + hm.GetComponent<HoleManager>().waterFill);
             hm.GetComponent<HoleManager>().insideHole.Add(col.gameObject);
