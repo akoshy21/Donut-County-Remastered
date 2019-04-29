f365907f60967f65ef7f8870912d66fded404bbf
Unknown <asc656@nyu.edu>
2019-04-29 15:24:19 -0400

    pause screen visuals

--- DC Prototype/Assets/Scripts/PauseButtons.cs
+++ DC Prototype/Assets/Scripts/PauseButtons.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 
 public class PauseButtons : MonoBehaviour
 {
+
+    Vector2 currentSize;
     
 
     private void Start()
     {
+        currentSize = GetComponent<RectTransform>().sizeDelta = new Vector2();
         
     }
 
     private void OnMouseOver()
     {
-        GetComponent<RectTransform>().sizeDelta = new Vector2();
+        currentSize = new Vector2(currentSize.x * 1.5f, currentSize.y * 1.5f);
     }
 }
