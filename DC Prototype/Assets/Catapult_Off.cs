40c9d029eee49a6f1068b7227bf65e5346e2ca3d
Unknown <rmh472@nyu.edu>
2019-04-29 19:39:15 -0400

    Mira Model Rigged

---
+++ DC Prototype/Assets/Catapult_Off.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult_Off : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Off()
+    {
+        GetComponent<Animator>().enabled = false;
+        gameObject.SetActive(false);  
+    }
+}
