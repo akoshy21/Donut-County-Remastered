944c9cf4e02b2c691429ec962857411bfd082c42
Unknown <asc656@nyu.edu>
2019-05-02 10:20:37 -0400

    rotating prisms

---
+++ DC Prototype/Assets/Scripts/Pause Screen/Prisms.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prisms : MonoBehaviour
+{
+    private Transform myT;
+    public float speed, speed1, speed2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myT = GetComponent<Transform>();
+        speed = Random.Range(speed1, speed2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myT.Rotate(new Vector3((speed * Time.deltaTime), (speed * Time.deltaTime), (speed * Time.deltaTime)));
+    }
+}
