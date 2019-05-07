767a44faba139a5d72448f97eb2443b25a30cc80
Annamaria Koshy <ak6429@nyu.edu>
2019-05-07 12:04:55 -0400

    readded ui elements

--- DC Prototype/Assets/Scripts/textRenderer.cs
+++ DC Prototype/Assets/Scripts/textRenderer.cs
@@ -128,7 +128,10 @@ public class textRenderer : MonoBehaviour
             }
             else
             {
+
                 SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("StartAdditive", LoadSceneMode.Additive);
+
             }
         }
 
