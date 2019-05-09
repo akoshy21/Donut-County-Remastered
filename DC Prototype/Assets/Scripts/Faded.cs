eccf00831e49cbd5de930985966e7738df240ba6
Annamaria Koshy <ak6429@nyu.edu>
2019-05-09 11:20:48 -0400

    General cutscene stuff now working

---
+++ DC Prototype/Assets/Scripts/Faded.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Faded : MonoBehaviour
+{
+    public bool fade;
+    public Image img;
+
+    private void Start()
+    {
+        img = this.GetComponent<Image>();
+    }
+
+    public IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(0, 0, 0, i);
+                Debug.Log("FADE");
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+        }
+    }
+}
--- DC Prototype/Assets/Scripts/IntroAndCamera/StartGame.cs
+++ DC Prototype/Assets/Scripts/IntroAndCamera/StartGame.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame : StateMachineBehaviour
 {
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
-        GameManager.manager.oof = true;
+        if (SceneManager.GetActiveScene().name.Equals("cutscene test"))
+        {
+            GameManager.manager.oof = true;
+            GameManager.manager.cutscene = false;
+        }
+        else {
+            GameManager.manager.dialogueCanvas.gameObject.SetActive(false); 
+        }
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
