ff3f5e9df2684839160ee4b57a0c8655bc56add7
Annamaria Koshy <ak6429@nyu.edu>
2019-05-14 02:20:30 -0400

    started pocket (ui element to tell whats in the hole)

---
+++ DC Prototype/Assets/Scripts/Pocket.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pocket : MonoBehaviour
+{
+    public GameObject hole;
+    public Image circle;
+    GameObject refModel = null;
+    public float h, w;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        h = Camera.main.orthographicSize * 2;
+        w = h * Screen.width / Screen.height; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPocket();
+        if(refModel !=null)
+        {
+            refModel.transform.Rotate(0, 50 * Time.deltaTime, 0);
+}
+    }
+
+    void CheckPocket()
+    {
+        if(hole.GetComponent<HoleManager>().insideHole.Count > 0)
+        {
+            circle.gameObject.SetActive(true);
+            if (refModel != null && !refModel.name.Equals(hole.GetComponent<HoleManager>().insideHole[hole.GetComponent<HoleManager>().insideHole.Count - 1].name))
+            {
+                Destroy(refModel.gameObject); 
+            }
+            refModel = Instantiate(hole.GetComponent<HoleManager>().insideHole[hole.GetComponent<HoleManager>().insideHole.Count - 1], circle.transform);
+                    
+        }
+        else {
+            circle.gameObject.SetActive(false);
+            }
+    }
+}
