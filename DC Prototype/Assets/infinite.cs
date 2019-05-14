7acb1f39a72b24b897492bf2e965ca7834b6cce9
Annamaria Koshy <ak6429@nyu.edu>
2019-05-14 14:04:13 -0400

    audio & water changes

--- DC Prototype/Assets/infinite.cs
+++ DC Prototype/Assets/infinite.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class infinite : MonoBehaviour
 {
-    private void OnDestroy()
+    private void OnCollisionEnter(Collision collision)
     {
         Instantiate(this);
     }
