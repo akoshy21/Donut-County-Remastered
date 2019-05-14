aaf3b5f4f94438df22315b595d23b7f50ae09287
Annamaria Koshy <ak6429@nyu.edu>
2019-05-14 13:53:34 -0400

    Here we go

--- DC Prototype/Assets/Restar.cs
+++ DC Prototype/Assets/Restar.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement();
+using UnityEngine.SceneManagement;
 
 public class Restar : MonoBehaviour
 {
-void Click()
-    {
-        Application.LoadLevel();
-}
+    public void Click()
+        {
+            SceneManager.LoadScene("cutscene test");
+    }
 }
