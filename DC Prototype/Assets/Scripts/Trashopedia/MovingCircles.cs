d623bf793fd6bf4cbf29606b37456cf51902a427
Unknown <asc656@nyu.edu>
2019-05-14 04:16:55 -0400

    scanner model & adding stuff to trasho

--- DC Prototype/Assets/Scripts/Trashopedia/MovingCircles.cs
+++ DC Prototype/Assets/Scripts/Trashopedia/MovingCircles.cs
@@ -8,6 +8,8 @@ public class MovingCircles : MonoBehaviour
     public Text objName;
     public Text objDesc;
 
+    public int speed;
+
     public Button right1;
     public Button right2;
     public Button left1;
@@ -47,7 +49,7 @@ public class MovingCircles : MonoBehaviour
 
         for (int i = 0; i < 7; i++) {
 
-            if (objs[i].GetComponent<RectTransform>().anchoredPosition.Equals(pos[first]))
+            if (objs[i].GetComponent<RectTransform>().anchoredPosition.Equals(pos[10]))
             {
                 objs[i].GetComponent<RawImage>().texture = mainImg;
             }
@@ -67,6 +69,8 @@ public class MovingCircles : MonoBehaviour
         {
             Debug.Log("obj" + j + "is at pos" + first);
             objs[j].GetComponent<RectTransform>().anchoredPosition = pos[i];
+            //this is supposed to lerp but like i cry 
+            //objs[j].GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(objs[j].GetComponent<RectTransform>().anchoredPosition, pos[i], speed, Time.deltaTime);
             j++;
         }
 
